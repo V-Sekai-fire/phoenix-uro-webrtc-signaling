d58508171d99c1889ded873e8b11c92c84f49aa2
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-07-16 09:32:02 -0700

    Update the protocol doc.

--- lib/uro_web/channels/lobby_channel.ex
+++ lib/uro_web/channels/lobby_channel.ex
@@ -4,28 +4,26 @@ defmodule UroWeb.LobbyChannel do
 
   ## Signaling Protocol
 
-  The protocol is JSON based, and uses messages in the form:
-
-  {
-    "id": "number",
-    "type": "number",
-    "data": "string"
-  }
-
-  With `type` being the message type, `id` being a connected peer or `0`, and `data` being the message specific data.
-
   Messages are the following:
 
-  - `0 = JOIN`: Must be sent by client immediately after connection to get a lobby assigned or join a known one (via the `data` field). This message is also sent by server back to the client to notify the assigned lobby, or simply a successful join.
-  - `1 = ID`: Sent by server to identify the client when it joins a room (the `id` field will contain the assigned ID).
-  - `2 = PEER_CONNECT`: Sent by server to notify new peers in the same lobby (the `id` field will contain the ID of the new peer).
-  - `3 = PEER_DISCONNECT`: Sent by server to notify when a peer in the same lobby disconnects (the `id` field will contain the ID of the disconnected peer).
-  - `4 = OFFER`: Sent by the client when creating a WebRTC offer then relayed back by the server to the destination peer.
-  - `5 = ANSWER`: Sent by the client when creating a WebRTC answer then relayed back by the server to the destination peer.
-  - `6 = CANDIDATE`: Sent by the client when generating new WebRTC candidates then relayed back by the server to the destination peer.
-  - `7 = SEAL`: Sent by client to seal the lobby (only the client that created it is allowed to seal a lobby), and then back by the server to notify success. When a lobby is sealed, no new client will be able to join, and the lobby will be destroyed (and clients disconnected) after 10 seconds.
+  | Type | Name            | Description                                                                                                                                                                                                 |
+  |------|-----------------|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+  | 0    | JOIN            | Must be sent by client immediately after connection to get a lobby assigned or join a known one (via the `data` field). This message is also sent by server back to the client to notify the assigned lobby, or simply a successful join.           |
+  | 1    | ID              | Sent by server to identify the client when it joins a room (the `id` field will contain the assigned ID).                                                                                                                                          |
+  | 2    | PEER_CONNECT    | Sent by server to notify new peers in the same lobby (the `id` field will contain the ID of the new peer).                                                                                                                                         |
+  | 3    | PEER_DISCONNECT | Sent by server to notify when a peer in the same lobby disconnects (the `id` field will contain the ID of the disconnected peer).                                                                                                                  |
+  | 4    | OFFER           | Sent by the client when creating a WebRTC offer then relayed back by the server to the destination peer.                                                                                                                                           |
+  | 5    | ANSWER          | Sent by the client when creating a WebRTC answer then relayed back by the server to the destination peer.                                                                                                                                          |
+  | 6    | CANDIDATE       | Sent by the client when generating new WebRTC candidates then relayed back by the server to the destination peer.                                                                                                                                  |
+  | 7    | SEAL            | Sent by client to seal the lobby (only the client that created it is allowed to seal a lobby), and then back by the server to notify success. When a lobby is sealed, no new client will be able to join, and the lobby will be destroyed (and clients disconnected) after 10 seconds. |
 
   For relayed messages (i.e., for `OFFER`, `ANSWER`, and `CANDIDATE`), the client will set the `id` field as the destination peer, then the server will replace it with the id of the sending peer, and send it to the proper destination.
+
+  ## Message Fields
+
+  - `id`: `number` - Represents a connected peer or `0`.
+  - `type`: `number` - Represents the message type.
+  - `data`: `string` - Contains the message-specific data.
   """
 
   use UroWeb, :channel
